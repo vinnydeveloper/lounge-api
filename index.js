aa1875960fa3fbf6b2ec2af78610b79724b6fab2
Vinicius Oliveira <vinicius.developer@outlook.com>
2023-02-15 13:13:07 -0300

    new build

--- index.js
+++ index.js
@@ -1,4 +1,31 @@
-const app = require('./src/app')
+const express = require("express");
+const routes = require("./src/routes");
+const cors = require("cors");
 
+const db = require("./src/database");
+const { ValidationError } = require("express-validation");
 
-app.listen(4000)
+const app = express();
+
+db.connect();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(routes);
+
+app.use((err, req, res, next) => {
+  if (err instanceof ValidationError) {
+    return res.status(err.statusCode).json(err);
+  }
+
+  return res.status(500).json(err);
+});
+
+const PORT = 4000
+
+app.listen(PORT, () => {
+  console.log(`API listening on PORT ${PORT} `)
+})
+
+module.exports = app
