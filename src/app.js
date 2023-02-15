b2ac095edc8c3a6566dfad9d16663bf41bbdc95f
Vinicius Oliveira <vinicius.developer@outlook.com>
2023-02-15 12:19:52 -0300

    new build

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ db.connect();
 app.use(cors());
 app.use(express.json());
 
-app.use(routes);
+app.use('/.netlify/functions/api',routes);
 
 app.use((err, req, res, next) => {
   if (err instanceof ValidationError) {
